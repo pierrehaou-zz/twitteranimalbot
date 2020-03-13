204e3fd591900aa27c717e10b70ec1833b4c6e1a
Pierre Haou <pierrehaou@gmail.com>
2020-03-12 19:22:53 -0700

    updated unsplashbot.py with sleep function

--- unsplashbot.py
+++ unsplashbot.py
@@ -1,13 +1,19 @@
 import config
 import unsplashapirequest
+import time
 
+#setting variable for sleep function
+downtime = 60 * 60 * 6
 
-# creates twitter api object
-api = config.create_api()
+while True:
+	# creates twitter api object
+	api = config.create_api()
 
-# downloads random animal photo from Unsplash API, returns source link
-source_text = unsplashapirequest.download_photo()
+	# downloads random animal photo from Unsplash API, returns source link
+	source_text = unsplashapirequest.download_photo()
 
-# Posts tweet to twitter with both the photo and photo source
-api.update_with_media(
-    "uphoto.jpg", status=f"This photo can be found at {source_text}")
+	# Posts tweet to twitter with both the photo and photo source
+	api.update_with_media(
+	    "uphoto.jpg", status=f"This photo can be found at {source_text}")
+	# Sets function to run four times a day
+	time.sleep(downtime)
