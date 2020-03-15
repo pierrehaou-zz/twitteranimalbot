20aaf47b755085581b90c50e8fefe70655fa8919
Pierre Haou <pierrehaou@gmail.com>
2020-03-15 10:51:58 -0700

    removed sleep functionality

--- unsplashbot.py
+++ unsplashbot.py
@@ -2,19 +2,15 @@ import config
 import unsplashapirequest
 import time
 
-#setting variable for sleep function
-downtime = 300
 
-while True:
-	# creates twitter api object
-	api = config.create_api()
+# creates twitter api object
+api = config.create_api()
 
-	# downloads random animal photo from Unsplash API, returns source link
-	source_text = unsplashapirequest.download_photo()
+# downloads random animal photo from Unsplash API, returns source link
+source_text = unsplashapirequest.download_photo()
 
-	# Posts tweet to twitter with both the photo and photo source
-	api.update_with_media(
+# Posts tweet to twitter with both the photo and photo source
+api.update_with_media(
 	    "uphoto.jpg", status=f"This photo can be found at {source_text}")
 
-	# Sets function to run four times a day
-	time.sleep(downtime)
+	
